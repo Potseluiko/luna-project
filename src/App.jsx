3ca0b9fc52c008decf4d5fc5cec690e347485c56
Aleksandra Ilyashenka <a.potseluiko@gmail.com>
2025-01-25 10:08:49 +0100

    add prettier + format all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import { useState } from 'react'
-import lunaImg from '/luna.svg'
-import './App.css'
+import lunaImg from "/luna.svg"
+import "./App.css"
 
 import LoginFormController from "./LoginFormController/LoginFormController.jsx"
 
-
 function App() {
-
-    return (
-        <div className="root">
-            <div className="header">
-                <div className="logo-container">
-                    <img src={lunaImg} className="logo-image" alt="Luna logo"/>
-                </div>
-                <div className="logo-text">Luna Project</div>
-            </div>
-            <div className="contet">
-                <LoginFormController/>
-            </div>
+  return (
+    <div className="root">
+      <div className="header">
+        <div className="logo-container">
+          <img src={lunaImg} className="logo-image" alt="Luna logo" />
         </div>
-    )
+        <div className="logo-text">Luna Project</div>
+      </div>
+      <div className="contet">
+        <LoginFormController />
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/LoginForm/LoginForm.jsx
+++ src/LoginForm/LoginForm.jsx
@@ -1,41 +1,41 @@
-import './LoginForm.css'
+import "./LoginForm.css"
 
 function LoginForm(props) {
+  const onChange = (event) => {
+    props.onChange({
+      [event.target.name]: event.target.value,
+    })
+  }
 
-    const onChange = (event) => {
-        props.onChange({
-            [event.target.name]: event.target.value
-        })
-    }
+  return (
+    <div className="login-form">
+      <div className="login-form__title">Sign in</div>
 
-    return (
-        <div className="login-form">
-            <div className="login-form__title">
-                Sign in
-            </div>
-
-            <form onSubmit={props.onSubmit}>
-                <div className="login-form__field">
-                    <input value={props.email} type="text" name="email" placeholder="Email" onChange={onChange}/>
-                </div>
-                <div className="login-form__field">
-                    <input value={props.password} type="password" name="password" placeholder="Password"
-                           onChange={onChange}/>
-                </div>
-                <div>
-                    <button type="submit">Sign in</button>
-                </div>
-                <div className="login-form__footer">
-                    <p>
-                        <a href="/register" className="login-form__link">Sign up</a>
-                    </p>
-                    <p>
-                        <a href="/forgot-password" className="login-form__link">Forgot your password??</a>
-                    </p>
-                </div>
-            </form>
+      <form onSubmit={props.onSubmit}>
+        <div className="login-form__field">
+          <input value={props.email} type="text" name="email" placeholder="Email" onChange={onChange} />
         </div>
-    )
+        <div className="login-form__field">
+          <input value={props.password} type="password" name="password" placeholder="Password" onChange={onChange} />
+        </div>
+        <div>
+          <button type="submit">Sign in</button>
+        </div>
+        <div className="login-form__footer">
+          <p>
+            <a href="/register" className="login-form__link">
+              Sign up
+            </a>
+          </p>
+          <p>
+            <a href="/forgot-password" className="login-form__link">
+              Forgot your password??
+            </a>
+          </p>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default LoginForm
