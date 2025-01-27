7ce9d8d9b0faad88f09e43db65c82acde8861e9c
Aleksandra Ilyashenka <a.potseluiko@gmail.com>
2025-01-27 20:15:59 +0100

    add mixins for fonts and some colors, improve styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import App from "./App/index.jsx"
-import "./index.scss"
+import "./styles/index.scss"
+import App from "./App"
 
 async function enableMocking() {
   // if (process.env.NODE_ENV !== 'development') {
