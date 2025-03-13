1307374cddca59ca4b084f909e0393ae2767eafd
Aleksandra Ilyashenka <a.potseluiko@gmail.com>
2025-03-13 21:55:03 +0200

    px to rem

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,14 +5,16 @@ import classes from "./App.module.scss"
 function App() {
   return (
     <div className={classes.root}>
-      <div className={classes.header}>
-        <div className={classes.logo}>
-          <img src={lunaImg} className={classes["logo-image"]} alt="Luna Project logo" />
-          <div className={classes["logo-text"]}>Luna Project</div>
+      <div className={classes.container}>
+        <div className={classes.header}>
+          <div className={classes.logo}>
+            <img src={lunaImg} className={classes["logo-image"]} alt="Luna Project logo" />
+            <div className={classes["logo-text"]}>Luna Project</div>
+          </div>
         </div>
-      </div>
-      <div className={classes.content}>
-        <SignInFormController />
+        <div className={classes.content}>
+          <SignInFormController />
+        </div>
       </div>
     </div>
   )
